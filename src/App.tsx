d25965125e065e57f8de7001d316a275ff9e66cf
unknown <ikhlak789@gmail.com>
2019-04-04 01:19:52 +0530

    changed folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 
 import './App.scss';
-import GloblaSettings from './Components/GlobalSettings/GlobalSettings'
+import GloblaSettings from './containers/GlobalSettings/GlobalSettings'
 
 class App extends React.Component {
   public render() {
