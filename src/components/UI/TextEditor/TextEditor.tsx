40a71321fd15e115a06fcad4e7b5bcf37eb1b087
unknown <ikhlak789@gmail.com>
2019-04-06 02:29:29 +0530

    Implemented and styled editor

--- src/components/UI/TextEditor/TextEditor.tsx
+++ src/components/UI/TextEditor/TextEditor.tsx
@@ -1,12 +1,49 @@
 import * as React from 'react';
-import 'quill';
+
+import './TextEditor.scss';
+import ReactQuill from 'react-quill';
 
 class TextEditor extends React.Component{
-  
+  public state={
+    text: ''
+  }
+
+  constructor(props:any) {
+    super(props)
+    this.state = { text: '' } // You can also pass a Quill Delta here
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  public handleChange(value:any) {
+    this.setState({ text: value })
+  }
+
   public render(){
+    const modules = {
+      toolbar: [
+        ['bold', 'italic', 'underline','strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'}],
+        ['link', 'image'],
+        ['clean'],
+        [{ 'header': [1, 2, 3, 4, false] }],
+      ],
+    };
+  
+    const formats = [
+      'header',
+      'bold', 'italic', 'underline', 'strike', 'blockquote',
+      'list', 'bullet', 'indent',
+      'link', 'image'
+    ];
+
     return (
       <div>
-        Hello
+        <ReactQuill
+          formats={formats}
+          modules={modules} 
+          value={this.state.text}
+          onChange={this.handleChange}
+          theme="snow"/> 
       </div>
     )
   }
