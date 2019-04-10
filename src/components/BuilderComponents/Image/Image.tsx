15f1283b1d4df6c1165af5146dcb61e2626bed3e
unknown <ikhlak789@gmail.com>
2019-04-10 17:40:33 +0530

    added Image Component

---
+++ src/components/BuilderComponents/Image/Image.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+ 
+const Image = () => {
+  return(
+    <React.Fragment>
+      <Row>
+        <Col>
+          <h3>Image</h3>
+          <form>
+            <Form.Group>
+              <Form.Label>Image URL</Form.Label>
+              <Form.Control type="text" placeholder="https://imgur.com/img.jpg" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Image Caption</Form.Label>
+              <Form.Control type="text" placeholder="A beautiful kitten" />
+            </Form.Group>
+            <Form.Group>
+              <Button>Add Image</Button>
+            </Form.Group>
+          </form>
+        </Col>
+      </Row>
+    </React.Fragment>
+  )
+}
+
+export default Image;
