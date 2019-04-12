0665e128545de2b70a33827cc0d3be5767e7525d
unknown <ikhlak789@gmail.com>
2019-04-12 16:08:00 +0530

    added new component exports

--- src/components/BuilderComponents.ts
+++ src/components/BuilderComponents.ts
@@ -1,1 +1,4 @@
 export { default as TextEditor } from './UI/TextEditor/TextEditor';
+export { default as Image } from './BuilderComponents/Image';
+export { default as Video } from './BuilderComponents/Video';
+export { default as Button } from './BuilderComponents/Button';
