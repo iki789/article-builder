b2f8a946615992d79fe1d2b9a3d52bb106ce53a1
unknown <ikhlak789@gmail.com>
2019-04-12 16:20:29 +0530

    fixed BuilderComponents structure

--- src/components/BuilderComponents.ts
+++
@@ -1,4 +0,0 @@
-export { default as TextEditor } from './UI/TextEditor/TextEditor';
-export { default as Image } from './BuilderComponents/Image';
-export { default as Video } from './BuilderComponents/Video';
-export { default as Button } from './BuilderComponents/Button';
