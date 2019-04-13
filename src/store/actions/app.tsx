324bdda9745e4cf7570d0d40f9b58c916a26a6d8
unknown <ikhlak789@gmail.com>
2019-04-13 16:09:03 +0530

    added show/hide modal actions

---
+++ src/store/actions/app.tsx
@@ -0,0 +1,13 @@
+import * as actionTypes from './actionTypes';
+
+export const HIDE_MODAL = () => {
+  return {
+    type: actionTypes.HIDE_MODAL
+  }
+}
+
+export const SHOW_MODAL = () => {
+  return {
+    type: actionTypes.SHOW_MODAL
+  }
+}
