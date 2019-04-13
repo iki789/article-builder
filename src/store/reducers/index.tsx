10a1998a97178f627e830af0dd3dbbcf5e4a80e1
unknown <ikhlak789@gmail.com>
2019-04-13 16:11:05 +0530

    added show/hide modal methods

--- src/store/reducers/index.tsx
+++ src/store/reducers/index.tsx
@@ -1,10 +1,30 @@
-const initialState = {
-  fonts: 'sabo'
+import * as ActionTypes from '../actions/actionTypes';
+
+const initialState:IAppState = {
+  showModal: false
 }
 
 const Reducer = (state = initialState, action:{type: string, payload: any}) => {
-  if(action.type){return state;}
+  if(action.type === ActionTypes.HIDE_MODAL){
+    state = {
+      ...state,
+      showModal: false
+    }
+  }
+
+  if(action.type === ActionTypes.SHOW_MODAL){
+    state = {
+      ...state,
+      showModal: true
+    }
+  }
+
   return state;
+}
+
+
+export interface IAppState{
+  showModal: boolean
 }
 
 export default Reducer;
