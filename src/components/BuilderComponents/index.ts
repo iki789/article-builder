003f06077e5386f4ba266f1b4b009c05c44fe79b
unknown <ikhlak789@gmail.com>
2019-04-16 02:50:15 +0530

    created ComponentMount component

--- src/components/BuilderComponents/index.ts
+++ src/components/BuilderComponents/index.ts
@@ -2,3 +2,4 @@ export { default as TextEditor } from '../UI/TextEditor/TextEditor';
 export { default as Image } from './Image';
 export { default as Video } from './Video';
 export { default as Button } from './Button';
+export { default as ComponentMount } from './ComponentMount';
