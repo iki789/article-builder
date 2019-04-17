7775cefabe63f34d090f6955d22985754ba814b3
unknown <ikhlak789@gmail.com>
2019-04-17 20:17:21 +0530

    replaced default textEditor with builder specific Editor

--- src/components/BuilderComponents/index.ts
+++ src/components/BuilderComponents/index.ts
@@ -1,4 +1,4 @@
-export { default as TextEditor } from '../UI/TextEditor/TextEditor';
+export { default as TextEditor } from './TextEditor';
 export { default as Image } from './Image';
 export { default as Video } from './Video';
 export { default as Button } from './Button';
