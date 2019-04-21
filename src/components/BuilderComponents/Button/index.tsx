47b76ec8f9f8e91cbb4aa9280dcfd2f8f2cadbfd
unknown <ikhlak789@gmail.com>
2019-04-22 01:43:45 +0530

    added state

--- src/components/BuilderComponents/Button/index.tsx
+++ src/components/BuilderComponents/Button/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Row, Col, Form, Button as BButton } from 'react-bootstrap';
 
-export class ButtonControl extends React.Component<IButtonProps>{
+export class ButtonControl extends React.Component<IButtonProps, IButtonState>{
+  
+  public state: IButtonState = {
+    label: this.props.label,
+    url: this.props.url || "#",
+    type: this.props.type || "default"
+  }
+
+  public handleChange = (e: React.ChangeEvent<any>) => {
+    this.setState({
+      ...this.state,
+      label: e.target.value
+    })
+    return e.target.value;
+  }
+
   public render(){
     return (
       <React.Fragment>
@@ -11,7 +27,11 @@ export class ButtonControl extends React.Component<IButtonProps>{
           <form>
             <Form.Group controlId="ButtonLabel">
               <Form.Label>Label</Form.Label>
-              <Form.Control type="text" value={this.props.label} placeholder="Submit" />
+              <Form.Control 
+                type="text" 
+                value={this.state.label}
+                onChange={this.handleChange} 
+                placeholder="View now" />
             </Form.Group>
             <Form.Group controlId="ButtonLocation">
               <Form.Label>URL Location</Form.Label>
@@ -38,11 +58,20 @@ export class ButtonControl extends React.Component<IButtonProps>{
   }
 }
 
+connect()(ButtonControl);
+
 interface IButtonProps{
   label: string,
   url?: string,
   type?: 'default' | 'outlined',
   block?: boolean 
+}
+
+interface IButtonState{
+  label: string,
+  url?: string,
+  type?: 'default' | 'outlined',
+  block?: boolean
 }
 
 export const Button:React.StatelessComponent<IButtonProps> = (props) => {
