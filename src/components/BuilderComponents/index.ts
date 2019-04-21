2a4209cce1dc2c22774ec3f119d7d476a409c8d0
unknown <ikhlak789@gmail.com>
2019-04-22 01:15:13 +0530

    exported Button

--- src/components/BuilderComponents/index.ts
+++ src/components/BuilderComponents/index.ts
@@ -1,6 +1,6 @@
 export { default as TextEditor } from './TextEditor';
 export { Image, ImageForm } from './Image';
 export { default as Video } from './Video';
-export { default as Button } from './Button';
+export { ButtonControl, Button } from './Button';
 export { default as ComponentMount } from './ComponentMount';
 export { default as ComponentFormMount } from './ComponentFormMount';
