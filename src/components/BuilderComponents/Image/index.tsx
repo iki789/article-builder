c5eb5c23be6c910deb1cc666d01a9b6e017bdd13
unknown <ikhlak789@gmail.com>
2019-04-21 17:34:08 +0530

    added Image and ImageForm

--- src/components/BuilderComponents/Image/index.tsx
+++ src/components/BuilderComponents/Image/index.tsx
@@ -1,43 +1,86 @@
 import * as React from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import './Image.scss';
+import { ICol } from 'src/containers/Preview/Preview';
+import { UPDATE_COL } from '../../../store/actions/app';
 
-interface IImage{
+class ImageControl extends React.Component<IImageFormProps>{
+
+  public onUpdate = (payload: any) =>{
+    // const col = this.props.activeCol;
+    console.log(this.props);
+  }
+
+  public render(){
+    return(
+      <React.Fragment>
+        <Row>
+          <Col>
+            <h3>Image</h3>
+            <form>
+              <Form.Group controlId="ImageSrc">
+                <Form.Label>Image Source</Form.Label>
+                <Form.Control 
+                  type="text" 
+                  placeholder="https://imgur.com/img.jpg" 
+                  value={this.props.src} onChange={this.onUpdate} />
+              </Form.Group>
+              <Form.Group controlId="ImageCaption">
+                <Form.Label>Image Caption</Form.Label>
+                <Form.Control type="text" placeholder="A beautiful kitten" />
+              </Form.Group>
+              <Form.Group controlId="ImageURL">
+                <Form.Label>Image URL</Form.Label>
+                <Form.Control type="text" placeholder="" />
+              </Form.Group>
+              <Form.Group>
+                <Button>Add Image</Button>
+              </Form.Group>
+            </form>
+          </Col>
+        </Row>
+      </React.Fragment>
+    )
+  }
+}
+
+interface IImageFormProps{
+  src?: string,
+  caption?: string,
+  url?: string,
+  onUpdate?: (payload: any) => void,
+  activeCol: ICol
+}
+
+
+interface IImageProps{
   src?: string,
   caption?: string,
   url?: string
 }
 
-export const ImageForm: React.StatelessComponent<IImage> = (props:IImage) => {
-  return(
-    <React.Fragment>
-      <Row>
-        <Col>
-          <h3>Image</h3>
-          <form>
-            <Form.Group controlId="ImageSrc">
-              <Form.Label>Image Source</Form.Label>
-              <Form.Control type="text" placeholder="https://imgur.com/img.jpg" value={props.src} />
-            </Form.Group>
-            <Form.Group controlId="ImageCaption">
-              <Form.Label>Image Caption</Form.Label>
-              <Form.Control type="text" placeholder="A beautiful kitten" />
-            </Form.Group>
-            <Form.Group controlId="ImageURL">
-              <Form.Label>Image URL</Form.Label>
-              <Form.Control type="text" placeholder="" />
-            </Form.Group>
-            <Form.Group>
-              <Button>Add Image</Button>
-            </Form.Group>
-          </form>
-        </Col>
-      </Row>
-    </React.Fragment>
-  )
+const mapStateToProps = (state: any) => {
+  return {
+    activeCol: state.rootReducer.activeCol
+  }
 }
 
-export const Image: React.StatelessComponent<IImage> = (props:IImage) => {
-  let img = <img src={props.src} /> ;
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    onUpdate: (col: ICol) => dispatch(UPDATE_COL(col))
+  }
+}
+
+export const ImageForm = connect(mapStateToProps, mapDispatchToProps)(ImageControl);
+
+export const Image: React.StatelessComponent<IImageProps> = (props:IImageProps) => {
+  let img = (
+    <div className="image-component">
+      <img src={props.src} />
+      <p>Credits</p>
+    </div>
+  ) ;
   if(props.url){
     img = (
       <a href={props.url} target="_blank">
@@ -47,9 +90,9 @@ export const Image: React.StatelessComponent<IImage> = (props:IImage) => {
   }
 
   return(
-    <div>
+    <React.Fragment>
       { img }
-    </div>
+    </React.Fragment>
   );
 }
 
