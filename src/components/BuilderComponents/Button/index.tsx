b9c03a19f0a4a6cc99ff43b1c35a6912cb995eb2
unknown <ikhlak789@gmail.com>
2019-04-22 01:15:28 +0530

    added Button and ButtonControl

--- src/components/BuilderComponents/Button/index.tsx
+++ src/components/BuilderComponents/Button/index.tsx
@@ -1,39 +1,54 @@
 import React from 'react';
-import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Row, Col, Form, Button as BButton } from 'react-bootstrap';
 
-const ButtonCompnent = () => {
-  return (
-    <React.Fragment>
-    <Row>
-      <Col>
-        <h3>Button</h3>
-        <form>
-          <Form.Group controlId="ButtonLabel">
-            <Form.Label>Label</Form.Label>
-            <Form.Control type="text" placeholder="Submit" />
-          </Form.Group>
-          <Form.Group controlId="ButtonLocation">
-            <Form.Label>URL Location</Form.Label>
-            <Form.Control type="text" placeholder="https://foobar.com" />
-          </Form.Group>
-          <Form.Group controlId="ButtonType">
-            <Form.Label>Type</Form.Label>
-            <Form.Control as="select">
-              <option>Default</option>
-              <option>Outlined</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="ButtonFullWidth">
-            <Form.Check custom={true} type="checkbox" label="Button is full-width" />
-          </Form.Group>
-          <Form.Group>
-            <Button>Create</Button>
-          </Form.Group>
-        </form>
-      </Col>
-    </Row>
-  </React.Fragment>
-  );
+export class ButtonControl extends React.Component<IButtonProps>{
+  public render(){
+    return (
+      <React.Fragment>
+      <Row>
+        <Col>
+          <h3>Button</h3>
+          <form>
+            <Form.Group controlId="ButtonLabel">
+              <Form.Label>Label</Form.Label>
+              <Form.Control type="text" value={this.props.label} placeholder="Submit" />
+            </Form.Group>
+            <Form.Group controlId="ButtonLocation">
+              <Form.Label>URL Location</Form.Label>
+              <Form.Control type="text" placeholder="https://foobar.com" />
+            </Form.Group>
+            <Form.Group controlId="ButtonType">
+              <Form.Label>Type</Form.Label>
+              <Form.Control as="select">
+                <option>Default</option>
+                <option>Outlined</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="ButtonFullWidth">
+              <Form.Check custom={true} type="checkbox" label="Button is full-width" />
+            </Form.Group>
+            <Form.Group>
+              <BButton>Create</BButton>
+            </Form.Group>
+          </form>
+        </Col>
+      </Row>
+    </React.Fragment>
+    );
+  }
 }
 
-export default ButtonCompnent;
+interface IButtonProps{
+  label: string,
+  url?: string,
+  type?: 'default' | 'outlined',
+  block?: boolean 
+}
+
+export const Button:React.StatelessComponent<IButtonProps> = (props) => {
+  return (
+    <BButton>
+      {props.label}
+    </BButton>
+  );
+} 
