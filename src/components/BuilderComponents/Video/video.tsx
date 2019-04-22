a0a438e24bcce1f0d7cb02ac29edc672b11dcf5d
unknown <ikhlak789@gmail.com>
2019-04-22 17:16:29 +0530

    fixed video props

--- src/components/BuilderComponents/Video/video.tsx
+++ src/components/BuilderComponents/Video/video.tsx
@@ -15,7 +15,12 @@ class Video extends React.Component<IVideo>{
   public render(){
     return (
       <React.Fragment>
-        <video ref={this.ref} controls={true} width="100%" poster={require('./placeholder.svg')} title="Hellothere1">
+        <video 
+          ref={this.ref} 
+          controls={true} 
+          poster={this.props.poster ? this.props.poster : require('./placeholder.svg') } 
+          title={this.props.title}
+          width="100%">
         <source src={this.props.src} />
         Browser doesn't support this video, please upgrade
       </video>
