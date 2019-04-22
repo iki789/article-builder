868513491bf98d8629369886427481d135530c26
unknown <ikhlak789@gmail.com>
2019-04-22 15:57:08 +0530

    imported correct video component

--- src/components/BuilderComponents/ComponentFormMount.tsx
+++ src/components/BuilderComponents/ComponentFormMount.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextEditor, ImageForm, Video, ButtonControl } from './index';
+import { TextEditor, ImageForm, VideoControl, ButtonControl } from './index';
 
 const ComponentMount = (props:{type?: string, data?: React.ReactElement | any}) => {
   let toMountComponent = <TextEditor value={<div>Write something here</div>} />;
@@ -12,7 +12,7 @@ const ComponentMount = (props:{type?: string, data?: React.ReactElement | any}) 
       toMountComponent = <ImageForm src={props.data.src} />;
       break;
     case 'video':
-      toMountComponent = <Video />;
+      toMountComponent = <VideoControl />;
       break;
     case 'button':
       toMountComponent = <ButtonControl label={props.data.label} url={props.data.url} block={props.data.block} />;
--- src/components/BuilderComponents/index.ts
+++ src/components/BuilderComponents/index.ts
@@ -1,6 +1,6 @@
 export { default as TextEditor } from './TextEditor';
 export { Image, ImageForm } from './Image';
-export { default as Video } from './Video';
+export { VideoControl } from './Video';
 export { ButtonControl, Button } from './Button';
 export { default as ComponentMount } from './ComponentMount';
 export { default as ComponentFormMount } from './ComponentFormMount';
