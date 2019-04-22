5a4e09cd3be61d533d1eaedb77f3ce4d2ea5046c
unknown <ikhlak789@gmail.com>
2019-04-22 15:56:48 +0530

    changed class name and exported

--- src/components/BuilderComponents/Video/index.tsx
+++ src/components/BuilderComponents/Video/index.tsx
@@ -1,29 +1,31 @@
 import * as React from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
  
-const Video = () => {
-  return(
-    <React.Fragment>
-      <Row>
-        <Col>
-          <h3>Video</h3>
-          <form>
-            <Form.Group>
-              <Form.Label>Video Source Link</Form.Label>
-              <Form.Control type="text" placeholder="https://imgur.com/video.mp4" />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Video Title</Form.Label>
-              <Form.Control type="text" placeholder="A beautiful kitten" />
-            </Form.Group>
-            <Form.Group>
-              <Button>Add Video</Button>
-            </Form.Group>
-          </form>
-        </Col>
-      </Row>
-    </React.Fragment>
-  )
+class VideoForm extends React.Component{
+  public render() {
+    return(
+      <React.Fragment>
+        <Row>
+          <Col>
+            <h3>Video</h3>
+            <form>
+              <Form.Group>
+                <Form.Label>Video Source Link</Form.Label>
+                <Form.Control type="text" placeholder="https://imgur.com/video.mp4" />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Video Title</Form.Label>
+                <Form.Control type="text" placeholder="A beautiful kitten" />
+              </Form.Group>
+              <Form.Group>
+                <Button>Add Video</Button>
+              </Form.Group>
+            </form>
+          </Col>
+        </Row>
+      </React.Fragment>
+    )
+  }
 }
 
-export default Video;
+export const VideoControl = VideoForm;
