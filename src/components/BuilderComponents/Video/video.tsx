1d203e9c53403b1aa3b3d2900f8935bcd3cb554e
unknown <ikhlak789@gmail.com>
2019-04-23 16:55:49 +0530

    show video element only when src is filled

--- src/components/BuilderComponents/Video/video.tsx
+++ src/components/BuilderComponents/Video/video.tsx
@@ -13,17 +13,20 @@ class Video extends React.Component<IVideo>{
   }
 
   public render(){
-    return (
-      <React.Fragment>
-        <video 
-          ref={this.ref} 
-          controls={true} 
-          poster={this.props.poster ? this.props.poster : require('./placeholder.svg') } 
-          title={this.props.title}
-          width="100%">
+    const vid = (
+      <video 
+        ref={this.ref} 
+        controls={true} 
+        poster={this.props.poster} 
+        title={this.props.title}
+        width="100%">
         <source src={this.props.src} />
         Browser doesn't support this video, please upgrade
       </video>
+    );
+    return (
+      <React.Fragment>
+      {this.props.src ? vid : <img src={require('./placeholder.svg')} width="100%" />}
       {this.props.title ? <p>{this.props.title}</p> : null }
       </React.Fragment>
      )
