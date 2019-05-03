756ff50c269bef9d9dd8aaa0b68c823575be3875
unknown <ikhlak789@gmail.com>
2019-05-04 02:05:24 +0530

    exported TextViewer

--- src/components/BuilderComponents/index.ts
+++ src/components/BuilderComponents/index.ts
@@ -1,4 +1,4 @@
-export { default as TextEditor } from './TextEditor';
+export { TextEditor, TextViewer } from './Text';
 export { Image, ImageForm } from './Image';
 export { VideoControl, Video } from './Video';
 export { ButtonControl, Button } from './Button';
