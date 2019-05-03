93dc13ceb3c43a4085993efee406d51703b06161
unknown <ikhlak789@gmail.com>
2019-05-04 02:58:32 +0530

    added method to change heading colors for text

--- src/components/BuilderComponents/Text/viewer.tsx
+++ src/components/BuilderComponents/Text/viewer.tsx
@@ -8,7 +8,7 @@ class TextViewer extends React.Component<ITextViewerProps>{
   
   public componentWillUpdate(nextProps: ITextViewerProps){
     if(this.state.value !== nextProps.value){
-      this.state.value = nextProps.value;
+      this.state.value = this.parseHtmlStringToAddThemeColor(nextProps.value);
     }
   }
 
@@ -17,6 +17,18 @@ class TextViewer extends React.Component<ITextViewerProps>{
       <div dangerouslySetInnerHTML={{__html: this.state.value}} />
     )
   }
+
+  private parseHtmlStringToAddThemeColor = (HtmlString: string): string => {
+    const parsedStringToHtml = document.createElement('div');
+    parsedStringToHtml.innerHTML = HtmlString ;
+    const headings = parsedStringToHtml.querySelectorAll('h1, h2, h3, h4');
+    headings.forEach((heading: HTMLElement) => {
+      heading.style.color = "red";
+    });
+    // Return string
+    return parsedStringToHtml.outerHTML;
+  }
+
 }
 
 interface ITextViewerProps{
