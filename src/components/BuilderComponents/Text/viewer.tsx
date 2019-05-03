f0034ea7430121216d7f7760be56bbf2d2342e75
unknown <ikhlak789@gmail.com>
2019-05-04 03:13:23 +0530

    added theme color to TextViewer

--- src/components/BuilderComponents/Text/viewer.tsx
+++ src/components/BuilderComponents/Text/viewer.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 class TextViewer extends React.Component<ITextViewerProps>{
   
   public state = {
-    value: this.props.value
+    value: this.props.value,
+    theme: this.props.theme
   }
   
   public componentWillUpdate(nextProps: ITextViewerProps){
     if(this.state.value !== nextProps.value){
-      this.state.value = this.parseHtmlStringToAddThemeColor(nextProps.value);
+      this.state.value = this.addThemeColorToHtml(nextProps);
     }
   }
 
@@ -18,12 +19,12 @@ class TextViewer extends React.Component<ITextViewerProps>{
     )
   }
 
-  private parseHtmlStringToAddThemeColor = (HtmlString: string): string => {
+  private addThemeColorToHtml = (props: ITextViewerProps): string => {
     const parsedStringToHtml = document.createElement('div');
-    parsedStringToHtml.innerHTML = HtmlString ;
+    parsedStringToHtml.innerHTML = props.value ;
     const headings = parsedStringToHtml.querySelectorAll('h1, h2, h3, h4');
     headings.forEach((heading: HTMLElement) => {
-      heading.style.color = "red";
+      heading.style.color = props.theme;
     });
     // Return string
     return parsedStringToHtml.outerHTML;
@@ -32,7 +33,8 @@ class TextViewer extends React.Component<ITextViewerProps>{
 }
 
 interface ITextViewerProps{
-  value: string
+  value: string,
+  theme: string
 }
 
 export default TextViewer;
