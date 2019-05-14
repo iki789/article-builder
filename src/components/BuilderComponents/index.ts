58e1ad2aaf4c6657b787102d3544f385e555f606
unknown <ikhlak789@gmail.com>
2019-05-15 02:30:46 +0530

    export Embed and EmbedControl

--- src/components/BuilderComponents/index.ts
+++ src/components/BuilderComponents/index.ts
@@ -2,5 +2,6 @@ export { TextEditor, TextViewer } from './Text';
 export { Image, ImageForm } from './Image';
 export { VideoControl, Video } from './Video';
 export { ButtonControl, Button } from './Button';
+export { Embed, EmbedControl } from './Embed'
 export { default as ComponentMount } from './ComponentMount';
 export { default as ComponentFormMount } from './ComponentFormMount';
