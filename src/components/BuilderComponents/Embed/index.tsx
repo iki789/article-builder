25e824e714b7ebe51485c604c22b9837aab646a9
unknown <ikhlak789@gmail.com>
2019-05-15 02:29:18 +0530

    connected to redux and handle/dispatch on value change

--- src/components/BuilderComponents/Embed/index.tsx
+++ src/components/BuilderComponents/Embed/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Row, Col, Form } from 'react-bootstrap';
+import { ICol } from 'src/containers/Preview/Preview';
+import { UPDATE_COL } from 'src/containers/Preview/store/actions';
 
 export const Embed: React.FunctionComponent<IEmbed> =  (props: IEmbed) => {
   return (
@@ -13,15 +16,19 @@ interface IEmbed{
   code: string
 }
 
-class EmbedControl extends React.Component<IEmbed, IEmbedControlState>{
+class EmbedControl extends React.Component<IEmbedControlProps, IEmbedControlState>{
   
   public state = { 
-    value: this.props.code
+    value: this.props.activeCol.data.code
   }
 
   public handleChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
     this.setState({
       value: e.target.value
+    }, () => {
+      const col = this.props.activeCol;
+      col.data.code = this.state.value;
+      this.props.updateCol(col);
     })
   }
 
@@ -31,7 +38,11 @@ class EmbedControl extends React.Component<IEmbed, IEmbedControlState>{
         <Col>
           <Form.Group>
             <Form.Label>Code</Form.Label>
-            <Form.Control as="textarea" placeholder="Paste your code here" onChange={this.handleChange} value={this.state.value} />
+            <Form.Control 
+              as="textarea" 
+              placeholder="Paste your code here" 
+              onChange={this.handleChange} 
+              value={this.state.value} />
           </Form.Group>
         </Col>
       </Row>
@@ -39,8 +50,28 @@ class EmbedControl extends React.Component<IEmbed, IEmbedControlState>{
   }
 }
 
+interface IEmbedControlProps{
+  code: string,
+  activeCol: ICol,
+  updateCol: (col: ICol) => void
+}
+
 interface IEmbedControlState{
   value: string
 }
 
-export { EmbedControl };
+const mapStateToProps = (state:any) => {
+  return {
+    activeCol: state.rootReducer.activeCol
+  }
+}
+
+const mapDispatchToProps = (dispatch:any) => {
+  return {
+    updateCol: (col:ICol) => dispatch(UPDATE_COL({...col}))
+  }
+}
+
+const EmbedControlConnected = connect(mapStateToProps, mapDispatchToProps)(EmbedControl);
+
+export { EmbedControlConnected as EmbedControl };
